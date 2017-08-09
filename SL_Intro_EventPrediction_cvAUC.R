02bc57de67a1114ab03fa2ba65984b5ea976d9ab
ainaimi <ashley.naimi@pitt.edu>
2017-08-08 20:40:42 -0400

    Made changes based on comments from LB:
    	1) Increased sample size from 1K to 10K
    	2) Changed algorithm from earth and rpart to bayesglm and randomForest
    	3) Incorporated commetns from LB
    
    Program shows improved stability

--- SL_Intro_EventPrediction_cvAUC.R
+++ SL_Intro_EventPrediction_cvAUC.R
@@ -1,34 +1,61 @@
-# Learning SuperLearner 
+# R code to demonstrate implementation of Super Learner 
+#	for binary classification (Example 2) in
+#  "Stacked Generalization: An Introdution to Super Learning"
+#	by Ashley I. Naimi and Laura B. Balzer
+
+# load the relevant packages
 library(SuperLearner);library(data.table);library(nnls);library(mvtnorm)
 library(ranger);library(xgboost);library(splines);library(Matrix)
 library(ggplot2);library(xtable);library(pROC)
-expit<-function(x){1/(1+exp(-x))}
+
+setwd("~/Dropbox/Documents/Research/Papers/SuperLearnerIntro/")
 
 # EXAMPLE 2
-set.seed(1234)
-n=1000
+set.seed(123)
+n=10000
 sigma <- abs(matrix(runif(25,0,1), ncol=5))
 sigma <- forceSymmetric(sigma)
 sigma <- as.matrix(nearPD(sigma)$mat)
 x <- rmvnorm(n, mean=c(0,.25,.15,0,.1), sigma=sigma)
 modelMat<-model.matrix(as.formula(~ (x[,1]+x[,2]+x[,3]+x[,4]+x[,5])^3))
 beta<-runif(ncol(modelMat)-1,0,1)
-beta<-c(2,beta)
-mu <- 1-expit(modelMat%*%beta)
+beta<- c(2,beta)   #*************************************** LB: could change the intercept to make outcome more common
+mu <- 1-plogis(modelMat%*%beta)  # true underlying risk of the outcome
 y<-rbinom(n,1,mu)
+
 hist(mu);mean(y)
+
 x<-data.frame(x)
 D<-data.frame(x,y)
 
+# Specify the number of folds for V-fold cross-validation
 folds=5
-sl.lib <- c("SL.earth","SL.rpart")
+
+#-------------------------------------------------------------------------------
+# Fit using the SuperLearner Package
+#-------------------------------------------------------------------------------
+# Specify the outcome-for-prediction (y), the predictors (x), 
+#		family (for a binary outcome), measure of performance (1-AUC),
+#		the library (sl.lib), and number of folds
+sl.lib <- c("SL.bayesglm","SL.randomForest")
 fitY<-SuperLearner(Y=y,X=x,family="binomial",
                    method="method.AUC",
-                   newX=x,SL.library=sl.lib,
-                   cvControl=list(V=folds))
-fitY;y_pred<-predict(fitY,onlySL=T)$pred
-p<-data.frame(y=y,y_pred=y_pred)
-a<-roc(p$y, p$y_pred, direction="auto")
+                   SL.library=sl.lib,
+                   cvControl=list(V=folds) ) 
+
+# Note: for rare binary outcomes, consider using the stratifyCV option to 
+#		maintain roughly the same # of outcomes per fold
+# View the output: 'Risk' column returns the CV estimates of (1-AUC)
+#		'Coef' column gives the weights for the final SuperLearner (meta-learner)
+fitY
+
+# Obtain the predicted probability of the outcome
+y_pred<-predict(fitY, onlySL=T)$pred
+p <- data.frame(y=y, y_pred=y_pred)
+
+# Use the roc() function to obtain measures of performance for binary classification
+a <- roc(p$y, p$y_pred, direction="auto")
+# To plot the ROC curve, we need the sensitivity and specificity
 C<-data.frame(sens=a$sensitivities,spec=a$specificities)
 
 ggplot() + geom_step(data=C, aes(1-spec,sens),
@@ -39,84 +66,103 @@ ggplot() + geom_step(data=C, aes(1-spec,sens),
   labs(x = "1 - Specificity",y = "Sensitivity") +
   geom_abline(intercept=0,slope=1,col="gray")
 
-# manually coded superlearner with three algorithms
-## 1: split data into 5 groups
+#-------------------------------------------------------------------------------
+# Hand-coding Super Learner
+#-------------------------------------------------------------------------------
+## 1: split data into 5 groups for 5-fold cross-validation 
 head(D,10)
 splt<-split(D,1:folds)
-head(splt[[1]])
+# view the first 6 observations in the first fold
+head(splt[1])
 
-## 2: fit each of the 3 algorithms on training set
-m1<-lapply(1:folds,function(ii) earth(formula=y~., data=do.call(rbind,splt[-ii]), degree = 2,
-                                      nk = 21, penalty = 3, pmethod = "backward", nfold = 0, 
-                                      ncross = 1, minspan = 0, endspan = 0, 
-                                      glm = list(family = binomial)))
-m2<-lapply(1:folds,function(ii) rpart(y~.,method="class",data=do.call(rbind,splt[-ii])))
-#m3<-lapply(1:folds,function(ii) mean(do.call(rbind,splt[-ii])$y))
+#----------------------------
+## 2: the lapply() function is an efficient way to rotate through the folds to execute:
+#	(a) set the ii-th fold to be the validation set; (b) fit each algorithm on the training set; 
+#  (c) obtain the predicted outcomes for observations in the validation set;
+#  (d) estimate the estimated risk (1-AUC) for each fold
+#
+## 2b: fit each algorithm on the training set (but not the ii-th validation set)
+m1<-lapply(1:folds,function(ii) bayesglm(formula=y~.,data=do.call(rbind,splt[-ii]),family="binomial"))
+m2<-lapply(1:folds,function(ii) randomForest(formula=as.factor(y)~.,data=do.call(rbind,splt[-ii])))
 
-## 3: predict from each fit in validation set
+## 2c: obtain the predicted probability of the outcome for observation in the ii-th validation set
 p1<-lapply(1:folds,function(ii) predict(m1[[ii]],newdata=rbindlist(splt[ii]),type="response"))
-p2<-lapply(1:folds,function(ii) predict(m2[[ii]],newdata=rbindlist(splt[ii]))[,2])
-#p3<-lapply(1:folds,function(ii) m3[[ii]])
-#one<-function(a){1*(a>runif(length(a)))}
+p2<-lapply(1:folds,function(ii) predict(m2[[ii]],newdata=data.frame(rbindlist(splt[ii])),type="prob")[,2])
+
+# update dataframe 'splt' so that column1 is the observed outcome (y)
+#   column2 is the CV-predicted probability of the outcome from earth
+#   column3 is the CV-predicted probability of the outcome from rpart
+for(i in 1:folds){
+		splt[[i]]<-cbind(splt[[i]][,6],p1[[i]],p2[[i]] )
+}
+# view the first 6 observations in the first fold 
 head(splt[[1]])
-for(i in 1:folds){splt[[i]]<-cbind(splt[[i]][,6],p1[[i]],p2[[i]])}
+#******************************** maybe rename columns? 
 
-## 4: calculate CV risk for each method
-head(splt[[4]])
-mse1<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,2],labels=splt[[ii]][,1]))
-mse2<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,3],labels=splt[[ii]][,1]))
-#mse3<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,4],labels=splt[[ii]][,1]))
+## 2d: calculate CV risk for each method for the ii-th validation set
+# our loss function is the rank loss; so our risk is (1-AUC)
+#		use the AUC() function with input as the predicted outcomes and 'labels' as the true outcomes
+risk1<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,2], labels=splt[[ii]][,1]))    # CV-risk for bayesglm
+risk2<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,3], labels=splt[[ii]][,1]))		# CV-risk for rpart
 
-a<-rbind(cbind("earth",mean(do.call(rbind,mse1),na.rm=T)),
-      cbind("rpart",mean(do.call(rbind,mse2),na.rm=T)))
-      #,cbind("Mean",mean(do.call(rbind,mse3),na.rm=T)))
+#----------------------------
+## 3: average the estimated 5 risks across the folds to obtain 1 measure of performance for each algorithm
+a<-rbind(cbind("bayesglm",mean(do.call(rbind, risk1),na.rm=T)),
+      cbind("rF",mean(do.call(rbind, risk2),na.rm=T)))
+      
+# output a table of the CV-risk estimates
 xtable(a)
+# compare with the package output  ********************* check
+fitY; a
 
-## 5: minimize 1 - AUC
-X<-data.frame(do.call(rbind,splt),row.names=NULL);names(X)<-c("y","earth","rpart")
+#----------------------------
+## 4: estimate SL weights using the optim() function to minimize (1-AUC)
+X<-data.frame(do.call(rbind,splt),row.names=NULL);  names(X)<-c("y","bayesglm","rF")
 head(X)
 
 bounds = c(0, Inf)
-SL.r<-function(A,y,par){
+SL.r<-function(A, y, par){
   A<-as.matrix(A)
-  names(par)<-c("earth","rpart")
-  predictions <- A%*%as.matrix(par)
+  names(par)<-c("bayesglm","rF")
+  predictions <- crossprod(t(A),par)
   cvRisk <- 1 - AUC(predictions = predictions, labels = y)
 }
-init=(rep(1,2))
+init=(rep(1/2,2))
 fit <- optim(par=init, fn=SL.r, A=X[,2:3], y=X[,1], 
              method="L-BFGS-B",lower=bounds[1],upper=bounds[2])
 fit
 alpha<-fit$par/sum(fit$par)
+fitY; a
 alpha
-# the above coefficeints seem to be reversed??
 
-## 6: fit all algorithms to original data and generate predictions
-m1<-earth(formula=y~., data=D, degree = 2,
-      nk = 21, penalty = 3, pmethod = "backward", nfold = 0, 
-      ncross = 1, minspan = 0, endspan = 0, 
-      glm = list(family = binomial))
-m2<-rpart(y~.,method="class",data=D)
+#---------------------
+## 5a: fit all algorithms to original data
+m1<-bayesglm(formula=y~.,data=D,family="binomial")
+m2<-randomForest(formula=as.factor(y)~.,data=D)
 
-## 7: predict from each fit using all data
-p1<-predict(m1,newdata=D,type="response")
-p2<-predict(m2,newdata=D)[,2]
+## 5b: predict probabilities from each fit using all data
+p1<-predict(m1,newdata=D,type="response")  # bayesglm
+p2<-predict(m2,newdata=D,type="prob")[,2] #randomForest
 predictions<-cbind(p1,p2)
+head(predictions)
 
-## 8: take a weighted combination of predictions using nnls parameters as weights
+## 5c: for the observed data take a weighted combination of predictions using nnls coeficients as weights
 y_pred <- predictions%*%alpha
-
 p<-data.frame(y=y,y_pred=y_pred)
-## 9: verify that our work predicts similar results as actual SL function
+
+
+## #--------------------------------------------
+# verify that our work predicts similar results as SL package
 a<-roc(p$y, p$y_pred, direction="auto")
 C2<-data.frame(sens=a$sensitivities,spec=a$specificities)
 head(C2)
 
-pdf(file="~/Dropbox/Documents/Research/Papers/SuperLearnerIntro/figure2.pdf",height=4,width=5)
-cols <- c("SuperLearner"="red","Manual SuperLearner"="blue")
+#************ simplifying the filename ----- maybe add the level-0 fits? 
+pdf(file="figure2.pdf",height=4,width=5)
+cols <- c("SuperLearner Package"="red","Manual SuperLearner"="blue")
 ggplot() +
   geom_step(data=C, aes(1-spec,sens),color="blue",size=.75) +
-  geom_step(data=C2, aes(1-spec,sens),color="red",linetype=3,size=1) +
+  geom_step(data=C2, aes(1-spec,sens),color="red",linetype=2,size=.5) +
   #scale_colour_manual(name="",values=cols) +
   theme_light() + theme(legend.position=c(.8,.8)) +
   theme(panel.grid.major = element_blank(),
