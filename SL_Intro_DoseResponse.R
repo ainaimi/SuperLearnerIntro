7176670454fa4ae9fb2c1796675b5955af147565
ainaimi <ashley.naimi@pitt.edu>
2017-08-15 09:22:40 -0400

    Updated example analyses.

--- SL_Intro_DoseResponse.R
+++ SL_Intro_DoseResponse.R
@@ -1,102 +1,176 @@
-# Learning SuperLearner 
+# R code to demonstrate implementation of Super Learner 
+#	 to estimate a dose-response curve (Example 1) in
+#  "Stacked Generalization: An Introdution to Super Learning"
+#	 by Ashley I. Naimi and Laura B. Balzer
+
+# load the relevant packages
 library(SuperLearner);library(data.table);library(nnls);library(rmutil)
 library(ranger);library(xgboost);library(splines);library(Matrix)
 library(ggplot2);library(xtable)
-expit<-function(x){1/(1+exp(-x))}
 
+# This program uses GAMs with 5 degrees of freedom (df=5)
+# The default for the SuperLearner is df = 2
+# To change these defaults, we "source" the following program, 
+# taken from Eric Polley's GitHub site: https://github.com/ecpolley/SuperLearnerExtra
+source("~/SLwrappers/create.SL.gam.Wrapper.R")
+
+setwd("~/Dropbox/Documents/Research/Papers/SuperLearnerIntro/")
 
 # EXAMPLE 1
+# set the seed for reproducibility
 set.seed(12345)
+
+# generate the observed data
 n=1000
 x = runif(n,0,8)
 y = 5 + 4*sqrt(9 * x)*as.numeric(x<2) + as.numeric(x>=2)*(abs(x-6)^(2)) + rlaplace(n)
+
+# to plot the true dose-response curve, generate sequence of 'doses' from 0 to 8 at every 0.1,
+#	then generate the true outcome
 xl<-seq(0,8,.1)
 yl<-5 + 4 * sqrt(9 * xl)*as.numeric(xl<2) + as.numeric(xl>=2)*(abs(xl-6)^(2))
 
-D<-data.frame(x,y)
-Dl<-data.frame(xl,yl)
+D<-data.frame(x,y)  	# observed data
+Dl<-data.frame(xl,yl)   # for plotting the true dose-response curve
 
-source("~/SLwrappers/create.SL.gam.Wrapper.R")
-source("~/SLwrappers/create.SL.nnet.R")
+# Specify the number of folds for V-fold cross-validation
+folds= 5
+## split data into 5 groups for 5-fold cross-validation 
+## we do this here so that the exact same folds will be used in 
+## both the SL fit with the R package, and the hand coded SL
+index<-split(1:1000,1:folds)
+splt<-lapply(1:folds,function(ind) D[index[[ind]],])
+# view the first 6 observations in the first [[1]] and second [[2]] folds
+head(splt[[1]])
+head(splt[[2]])
+
+#-------------------------------------------------------------------------------
+# Fit using the SuperLearner Package
+#-------------------------------------------------------------------------------
+# Create the 5 df GAMs and size = 5 NNET using functions called from programs "sourced" above
 create.SL.gam(deg.gam = 5)
-create.SL.nnet(size=5)
-sl.lib <- c("SL.gam.5","SL.nnet.5")
-fitY<-SuperLearner(Y=y,X=data.frame(x),newX=data.frame(x),SL.library=sl.lib,cvControl=list(V=5))
+create.SL.nnet(size=4)
+
+# Specifying the SuperLearner library of candidate algorithms
+sl.lib <- c("SL.gam.5","SL.earth")
+
+# Fit using the SuperLearner package, specify 
+#		outcome-for-prediction (y), the predictors (x), the loss function (L2),
+#		the library (sl.lib), and number of folds 
+fitY<-SuperLearner(Y=y,X=data.frame(x), method="method.NNLS", SL.library=sl.lib,cvControl=list(V= folds,validRows=index))
+
+# View the output: 'Risk' column returns the CV-MSE estimates
+#		'Coef' column gives the weights for the final SuperLearner (meta-learner)
 fitY
+# Now predict the outcome for all possible x 'doses'
 yS<-predict(fitY,newdata=data.frame(x=xl),onlySL=T)$pred
 
+# Create a dataframe of all x 'doses' and predicted SL responses
 Dl1<-data.frame(xl,yS)
 
-# manually coded superlearner with three algorithms
-## 1: split data into 5 groups
-folds=5
-D<-data.frame(x,y)
-head(D,10)
-splt<-split(D,1:folds)
+#-------------------------------------------------------------------------------
+# Hand-coding Super Learner
+#-------------------------------------------------------------------------------
 
-## 2: fit each of the 3 algorithms on training set
+## 2: the lapply() function is an efficient way to rotate through the folds to execute the following:
+#	(a) set the ii-th fold to be the validation set; (b) fit each algorithm on the training set, 
+#     which is what's left after taking the ii-th fold out (i.e., splt[-ii]); 
+# (c) obtain the predicted outcomes for observations in the validation set;
+# (d) estimate the estimated risk (CV-MSE) for each fold
+#
+## 2b: fit each algorithm on the training set (but not the ii-th validation set)
 m1<-lapply(1:folds,function(ii) gam(y~s(x,5),family="gaussian",data=rbindlist(splt[-ii])))
-m2<-lapply(1:folds,function(ii) nnet(x=x,y=y,linout=T,trace=F,maxit=500,size=5,data=rbindlist(splt[-ii])))
+m2<-lapply(1:folds,function(ii) earth(y~x,data=rbindlist(splt[-ii]),degree = 2, penalty = 3, 
+                                      nk = 21, pmethod = "backward", nfold = 0, 
+                                      ncross = 1, minspan = 0, endspan = 0))
+#m2<-lapply(1:folds,function(ii) nnet(y~x,linout=T,trace=F,maxit=500,size=2,data=rbindlist(splt[-ii])))
 
-## 3: predict from each fit in validation set
+## 2c: predict the outcomes for observation in the ii-th validation set
 p1<-lapply(1:folds,function(ii) predict(m1[[ii]],newdata=rbindlist(splt[ii]),type="response"))
-p2<-lapply(1:folds,function(ii) predict(m2[[ii]],newdata=rbindlist(splt[ii]),type="raw"))
-for(i in 1:folds){splt[[i]]<-cbind(splt[[i]],p1[[i]],p2[[i]])}
+p2<-lapply(1:folds,function(ii) predict(m2[[ii]],newdata=rbindlist(splt[ii]),type="response"))
 
-## 4: calculate CV risk for each method (MSE??)
-mse1<-lapply(1:folds,function(ii) mean((splt[[ii]][,2]-splt[[ii]][,3])^2))
-mse2<-lapply(1:folds,function(ii) mean((splt[[ii]][,2]-splt[[ii]][,4])^2))
+# add the predictions to grouped dataset 'splt'
+for(i in 1:folds){
+	splt[[i]]<-cbind(splt[[i]],p1[[i]],p2[[i]])
+}
+# view the first 6 observations in the first fold 
+#   column2 (y) is the observed outcome;  
+#   column3 is the CV-predictions from gam
+#   column4 is the CV-predictions from nnet; 
+head(splt[[1]])
 
-a<-rbind(cbind("GAM",mean(do.call(rbind,mse1),na.rm=T)),
-      cbind("nnet",mean(do.call(rbind,mse2),na.rm=T)))
-xtable(a)
+## 2d: calculate CV risk for each method for with ii-th validation set
+# our loss function is L2-squared error; so our risk is mean squared error
+risk1<-lapply(1:folds,function(ii) mean((splt[[ii]][,2]-splt[[ii]][,3])^2))
+risk2<-lapply(1:folds,function(ii) mean((splt[[ii]][,2]-splt[[ii]][,4])^2))
 
-## 5: estimate SL weights using nnls (for convex combination) and normalize
-X<-data.frame(do.call(rbind,splt))[,-1];names(X)<-c("y","gam","nnet")
+## 3: average the estimated risks across the 5 folds to obtain 1 measure of performance for each algorithm
+a<-rbind(cbind("gam",mean(do.call(rbind,risk1),na.rm=T)),
+      cbind("nnet",mean(do.call(rbind,risk2),na.rm=T)))
+
+# checking to see match with SL output 
+fitY
+a
+
+#------------
+## 4: estimate SL weights using nnls (for convex combination) and normalize
+
+# create a new datafame with the observed outcome (y) and CV-predictions from the 3 algorithms
+X<-data.frame(do.call(rbind,splt))[,-1];names(X)<-c("y","gam","earth")
 head(X)
 SL.r<-nnls(cbind(X[,2],X[,3]),X[,1])$x
 alpha<-as.matrix(SL.r/sum(SL.r))
 round(alpha,3)
+# compare to the package's coefficients 
 fitY
-## 6: fit all algorithms to original data and generate predictions
-m1<-gam(y~s(x,5),family="gaussian",data=D)
-m2<-nnet(x=x,y=y,linout=T,trace=F,maxit=500,size=5,data=D)
 
-## 7: predict from each fit using all data
+#----------------
+## 5a: fit all algorithms to original data and generate predictions
+m1<-gam(y~s(x,5),family="gaussian",data=D)
+m2<-earth(y~x,data=D,degree = 2, penalty = 3, 
+          nk = 21, pmethod = "backward", nfold = 0, 
+          ncross = 1, minspan = 0, endspan = 0)
+
+## 5b: predict from each fit using all data
 p1<-predict(m1,newdata=D,type="response")
-p2<-predict(m2,newdata=D,type="raw")
+p2<-predict(m2,newdata=D,type="response")
+
 predictions<-cbind(p1,p2)
 
-## 8: take a weighted combination of predictions using nnls parameters as weights
+## 5c: for the observed data take a weighted combination of predictions using nnls coeficients as weights
 y_pred <- predictions%*%alpha
 
-## 9: verify that our work predicts similar results as actual SL function
+
+#--------------------------------------------
+## now apply to new dataset (all doses (xl)) to  verify that our work predicts similar results as actual SL function
 p1<-predict(m1,newdata=data.frame(x=xl),type="response")
-p2<-predict(m2,newdata=data.frame(x=xl),type="raw")
+p2<-predict(m2,newdata=data.frame(x=xl),type="response")
+
 predictions<-cbind(p1,p2)
 yS2 <- predictions%*%alpha
 
+# now we have a new dataframe of doses (xl) and SL manual predicted outcome
 Dl2<-data.frame(xl,yS2)
 
-pdf(file="~/Dropbox/Documents/Research/Papers/SuperLearnerIntro/figure1.pdf",height=4,width=5)
-cols <- c("Truth"="gray25","SuperLearner"="red","Manual SuperLearner"="blue")
+#------------------------------------
+# adding the predictions from the candidate algorithms
+Dgam = data.frame(xl, p1)
+Dearth = data.frame(xl, p2)
+
+pdf(file="figure1.pdf",height=4,width=5)
+cols <- c("Truth"="gray25","SuperLearner Package"="red","Manual SuperLearner"="blue", 
+	"gam"= "green4", "earth"="deepskyblue2")
 ggplot() +
   geom_point(data=D, aes(x,y),color="gray75",size=.75) + 
-  geom_line(data=Dl2, aes(xl,yS2,color="Manual SuperLearner"),size=1) + 
-  geom_line(data=Dl1, aes(xl,yS,color="SuperLearner"),size=1) + 
+  geom_line(data=Dl2, aes(xl,yS2,color="Manual SuperLearner"),size=.5) + 
+  geom_line(data=Dl1, aes(xl,yS,color="SuperLearner Package"),size=.5, linetype=2) + 
+   geom_line(data=Dgam, aes(xl,p1,color="gam"),size=.75, linetype=2) + 
+   geom_line(data=Dearth, aes(xl,p2,color="earth"),size=.75, linetype=2) + 
+
   geom_line(data=Dl, aes(xl,yl,color="Truth"),size=.5) + 
   scale_colour_manual(name="",values=cols) +
-  theme_light() + theme(legend.position=c(.8,.8)) +
+  theme_light() + theme(legend.position=c(.8,.75)) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) + 
   labs(x = "Exposure",y = "Outcome")
 dev.off()
-
-lines(xl,yS2,lwd=2,col="blue")
-legend("topleft",c("Real SL","Ashley's SL"),col=c("red","blue"),lty=c(1,1),lwd=c(2,2))
-
-plot(yS,yS2)
-lines(x = seq(min(yS),max(yS)), y = seq(min(yS),max(yS)),col="red")
-
-## EXAMPLE 2
-
--- SL_Intro_EventPrediction_cvAUC.R
+++ SL_Intro_EventPrediction_cvAUC.R
@@ -1,6 +1,6 @@
 # R code to demonstrate implementation of Super Learner 
 #	for binary classification (Example 2) in
-#  "Stacked Generalization: An Introdution to Super Learning"
+# "Stacked Generalization: An Introdution to Super Learning"
 #	by Ashley I. Naimi and Laura B. Balzer
 
 # load the relevant packages
@@ -8,6 +8,7 @@ library(SuperLearner);library(data.table);library(nnls);library(mvtnorm)
 library(ranger);library(xgboost);library(splines);library(Matrix)
 library(ggplot2);library(xtable);library(pROC)
 
+# set the working directory: e.g. 
 setwd("~/Dropbox/Documents/Research/Papers/SuperLearnerIntro/")
 
 # EXAMPLE 2
@@ -19,8 +20,8 @@ sigma <- as.matrix(nearPD(sigma)$mat)
 x <- rmvnorm(n, mean=c(0,.25,.15,0,.1), sigma=sigma)
 modelMat<-model.matrix(as.formula(~ (x[,1]+x[,2]+x[,3]+x[,4]+x[,5])^3))
 beta<-runif(ncol(modelMat)-1,0,1)
-beta<- c(2,beta)   #*************************************** LB: could change the intercept to make outcome more common
-mu <- 1-plogis(modelMat%*%beta)  # true underlying risk of the outcome
+beta<-c(2,beta) # setting intercept
+mu <- 1-plogis(modelMat%*%beta) # true underlying risk of the outcome
 y<-rbinom(n,1,mu)
 
 hist(mu);mean(y)
@@ -30,6 +31,14 @@ D<-data.frame(x,y)
 
 # Specify the number of folds for V-fold cross-validation
 folds=5
+## split data into 5 groups for 5-fold cross-validation 
+## we do this here so that the exact same folds will be used in 
+## both the SL fit with the R package, and the hand coded SL
+index<-split(1:1000,1:folds)
+splt<-lapply(1:folds,function(ind) D[index[[ind]],])
+# view the first 6 observations in the first [[1]] and second [[2]] folds
+head(splt[[1]])
+head(splt[[2]])
 
 #-------------------------------------------------------------------------------
 # Fit using the SuperLearner Package
@@ -37,11 +46,11 @@ folds=5
 # Specify the outcome-for-prediction (y), the predictors (x), 
 #		family (for a binary outcome), measure of performance (1-AUC),
 #		the library (sl.lib), and number of folds
-sl.lib <- c("SL.bayesglm","SL.randomForest")
+sl.lib <- c("SL.bayesglm","SL.polymars")
 fitY<-SuperLearner(Y=y,X=x,family="binomial",
                    method="method.AUC",
                    SL.library=sl.lib,
-                   cvControl=list(V=folds) ) 
+                   cvControl=list(V=folds))
 
 # Note: for rare binary outcomes, consider using the stratifyCV option to 
 #		maintain roughly the same # of outcomes per fold
@@ -52,17 +61,16 @@ fitY
 # Obtain the predicted probability of the outcome
 y_pred<-predict(fitY, onlySL=T)$pred
 p <- data.frame(y=y, y_pred=y_pred)
+head(p)
 
 # Use the roc() function to obtain measures of performance for binary classification
 a <- roc(p$y, p$y_pred, direction="auto")
 # To plot the ROC curve, we need the sensitivity and specificity
 C<-data.frame(sens=a$sensitivities,spec=a$specificities)
 
-ggplot() + geom_step(data=C, aes(1-spec,sens),
-                     color="blue",size=.25) + 
-  theme_light() + 
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) + 
+ggplot() + geom_step(data=C, aes(1-spec,sens),color="blue",size=.25) + 
+  theme_light() + theme(panel.grid.major = element_blank(),
+                        panel.grid.minor = element_blank()) + 
   labs(x = "1 - Specificity",y = "Sensitivity") +
   geom_abline(intercept=0,slope=1,col="gray")
 
@@ -73,7 +81,7 @@ ggplot() + geom_step(data=C, aes(1-spec,sens),
 head(D,10)
 splt<-split(D,1:folds)
 # view the first 6 observations in the first fold
-head(splt[1])
+head(splt[[1]])
 
 #----------------------------
 ## 2: the lapply() function is an efficient way to rotate through the folds to execute:
@@ -83,47 +91,44 @@ head(splt[1])
 #
 ## 2b: fit each algorithm on the training set (but not the ii-th validation set)
 m1<-lapply(1:folds,function(ii) bayesglm(formula=y~.,data=do.call(rbind,splt[-ii]),family="binomial"))
-m2<-lapply(1:folds,function(ii) randomForest(formula=as.factor(y)~.,data=do.call(rbind,splt[-ii])))
+m2<-lapply(1:folds,function(ii) polyclass(do.call(rbind,splt[-ii])[,6],do.call(rbind,splt[-ii])[,1:5],cv=5))
 
 ## 2c: obtain the predicted probability of the outcome for observation in the ii-th validation set
 p1<-lapply(1:folds,function(ii) predict(m1[[ii]],newdata=rbindlist(splt[ii]),type="response"))
-p2<-lapply(1:folds,function(ii) predict(m2[[ii]],newdata=data.frame(rbindlist(splt[ii])),type="prob")[,2])
+p2<-lapply(1:folds,function(ii) ppolyclass(fit=m2[[ii]],cov=rbindlist(splt[ii])[,1:5])[,2])
 
 # update dataframe 'splt' so that column1 is the observed outcome (y)
-#   column2 is the CV-predicted probability of the outcome from earth
-#   column3 is the CV-predicted probability of the outcome from rpart
+#   column2 is the CV-predicted probability of the outcome from bayesglm
+#   column3 is the CV-predicted probability of the outcome from random forest
 for(i in 1:folds){
-		splt[[i]]<-cbind(splt[[i]][,6],p1[[i]],p2[[i]] )
+		splt[[i]]<-cbind(splt[[i]][,6],p1[[i]],p2[[i]])
 }
 # view the first 6 observations in the first fold 
-head(splt[[1]])
-#******************************** maybe rename columns? 
 
 ## 2d: calculate CV risk for each method for the ii-th validation set
 # our loss function is the rank loss; so our risk is (1-AUC)
 #		use the AUC() function with input as the predicted outcomes and 'labels' as the true outcomes
 risk1<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,2], labels=splt[[ii]][,1]))    # CV-risk for bayesglm
-risk2<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,3], labels=splt[[ii]][,1]))		# CV-risk for rpart
+risk2<-lapply(1:folds,function(ii) 1-AUC(predictions=splt[[ii]][,3], labels=splt[[ii]][,1]))		# CV-risk for knn
 
 #----------------------------
 ## 3: average the estimated 5 risks across the folds to obtain 1 measure of performance for each algorithm
 a<-rbind(cbind("bayesglm",mean(do.call(rbind, risk1),na.rm=T)),
-      cbind("rF",mean(do.call(rbind, risk2),na.rm=T)))
-      
+      cbind("polymars",mean(do.call(rbind, risk2),na.rm=T)))
 # output a table of the CV-risk estimates
-xtable(a)
-# compare with the package output  ********************* check
-fitY; a
+# xtable(a)
+# compare with the package output
+fitY;a
 
 #----------------------------
 ## 4: estimate SL weights using the optim() function to minimize (1-AUC)
-X<-data.frame(do.call(rbind,splt),row.names=NULL);  names(X)<-c("y","bayesglm","rF")
+X<-data.frame(do.call(rbind,splt),row.names=NULL);  names(X)<-c("y","bayesglm","poly")
 head(X)
 
 bounds = c(0, Inf)
 SL.r<-function(A, y, par){
   A<-as.matrix(A)
-  names(par)<-c("bayesglm","rF")
+  names(par)<-c("bayesglm","poly")
   predictions <- crossprod(t(A),par)
   cvRisk <- 1 - AUC(predictions = predictions, labels = y)
 }
@@ -138,11 +143,11 @@ alpha
 #---------------------
 ## 5a: fit all algorithms to original data
 m1<-bayesglm(formula=y~.,data=D,family="binomial")
-m2<-randomForest(formula=as.factor(y)~.,data=D)
+m2<-polyclass(D[,6],D[,1:5],cv=5)
 
 ## 5b: predict probabilities from each fit using all data
 p1<-predict(m1,newdata=D,type="response")  # bayesglm
-p2<-predict(m2,newdata=D,type="prob")[,2] #randomForest
+p2<-ppolyclass(fit=m2,cov=D[,1:5])[,2] #randomForest
 predictions<-cbind(p1,p2)
 head(predictions)
 
@@ -150,23 +155,32 @@ head(predictions)
 y_pred <- predictions%*%alpha
 p<-data.frame(y=y,y_pred=y_pred)
 
-
 ## #--------------------------------------------
 # verify that our work predicts similar results as SL package
 a<-roc(p$y, p$y_pred, direction="auto")
 C2<-data.frame(sens=a$sensitivities,spec=a$specificities)
 head(C2)
 
-#************ simplifying the filename ----- maybe add the level-0 fits? 
+###--------------------------------------------
+# fits from candidate algorithms
+a<-roc(y, p1, direction="auto")
+Cbayes<-data.frame(sens=a$sensitivities,spec=a$specificities)
+
+a<-roc(y, p2, direction="auto") 
+Cpoly<-data.frame(sens=a$sensitivities,spec=a$specificities)
+
 pdf(file="figure2.pdf",height=4,width=5)
-cols <- c("SuperLearner Package"="red","Manual SuperLearner"="blue")
+cols <- c("SuperLearner Package"="red","Manual SuperLearner"="blue", "Bayes GLM"="green", "PolyMARS"="black")
 ggplot() +
-  geom_step(data=C, aes(1-spec,sens),color="blue",size=.75) +
-  geom_step(data=C2, aes(1-spec,sens),color="red",linetype=2,size=.5) +
+  geom_step(data=C, aes(1-spec,sens,color="Manual SuperLearner"),size=.75) +
+  geom_step(data=C2, aes(1-spec,sens,color="SuperLearner Package"),linetype=2,size=.5) +
+  geom_step(data=Cbayes, aes(1-spec,sens,color="Bayes GLM"),linetype=2,size=.5) +
+  geom_step(data=Cpoly, aes(1-spec,sens,color="PolyMARS"),linetype=2,size=.5) +
   #scale_colour_manual(name="",values=cols) +
-  theme_light() + theme(legend.position=c(.8,.8)) +
+  theme_light() + theme(legend.position=c(.8,.2)) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) + 
   labs(x = "1 - Specificity",y = "Sensitivity") +
-  geom_abline(intercept=0,slope=1,col="gray")
+  geom_abline(intercept=0,slope=1,col="gray") +
+  scale_colour_manual(name="",values=cols)
 dev.off()
